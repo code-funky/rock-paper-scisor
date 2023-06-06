95f6f8dc06e9c1e3595d8e3a153541496a2cdded
Ko <rickncode@gmail.com>
2023-06-06 21:13:14 +0800

    Add game function to click button to start game

--- script.js
+++ script.js
@@ -1,81 +1,71 @@
 
 // This function call return random choice of 'rock', 'paper', 'scissor'//
-function computerSelection () {
+function randomSelection() {
     let choice = ['rock', 'paper', 'scissor'];
     let randomChoice = choice[Math.floor(Math.random()*choice.length)];
     return randomChoice;
 }
 // End of function//
 
-// This function call compare player input and computer input and output result//
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return("It's a draw!");
-    } else if ( (playerSelection === "rock" && computerSelection === "scissor") ||
-                (playerSelection === "paper" && computerSelection === "rock") ||
-                (playerSelection === "scissor" && computerSelection === "paper")
-    ) {
-        return("You win!");
-    } else if ( (playerSelection === "rock" && computerSelection === "paper") ||
-                (playerSelection === "paper" && computerSelection === "scissor") ||
-                (playerSelection === "scissor" && computerSelection === "rock")
-    ) {
-        return("You lose!");
-    } else {return("Invalid input!")}
-}
-// End of function//
-
-
-
-// End of variable declaration//
-
-// This function display round score
-function displayRoundScore() {console.log(playRound(playerSelection, computerSelection));};
-// This function display total score
-function displayTotalScore() {console.log("Win: " + winCount + " Lose: " + loseCount);};
-// This function display 
-
 function game() {
     let winCount = 0;
     let loseCount = 0;
     for (let i = 0; i < 5; i++) {
-        // This function only accept one of the 3 valid value//
         let playerSelection = "";
-        let validInput = false;    
+        let computerSelection = randomSelection();
+        let validInput = false;
         while (!validInput) {
             playerSelection = prompt("Please input your choice", "");
+            
+            // This function call compare player input and computer input and output result//
+            function playRound(playerSelection, computerSelection) {
+                if (playerSelection === computerSelection) {
+                    return("It's a draw!");
+                } else if ( (playerSelection === "rock" && computerSelection === "scissor") ||
+                            (playerSelection === "paper" && computerSelection === "rock") ||
+                            (playerSelection === "scissor" && computerSelection === "paper")
+                ) {
+                    return("You win!");
+                } else 
+                  return("You lose!");
+            }
+            // End of function//
+
             if (playerSelection !== null && playerSelection !== "") {
                 playerSelection = playerSelection.toLowerCase();
                     if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissor") {
                         validInput = true;
+                        console.log("Round " + (i+1))
+                        console.log("Player: " + playerSelection)
+                        console.log("Computer: " + computerSelection)
+                        let result = playRound(playerSelection, computerSelection);
+                        console.log("Result: " + result)
+                        if (result === "You win!") {
+                            winCount++;
+                            console.log("You: " + winCount);
+                            console.log("Computer: " + loseCount)
+                        } else if (result === "You lose!") {
+                            loseCount++;
+                            console.log("You: " + winCount);
+                            console.log("Computer: " + loseCount)
+                        } else {
+                            console.log("You: " + winCount);
+                            console.log("Computer: " + loseCount)                            
+                        }
                     } else alert('Input must be either one of the following \nrock\npaper\nscissor')
+            } else if (playerSelection === null) {
+                console.log("Player has quit the game")
+                return;
             } else alert("Input can't be empty");
         } 
-        let result = playRound(playerSelection, computerSelection);
-        console.log("Player: " + playerSelection)
-        console.log("Computer: " + computerSelection)
-        console.log("Result: " + result)   
-    } if (result === "You win!") {
-        winCount++;
-    } else if (result === "You lose!") {
-        loseCount++;
-    } 
-    
+    }    
     if (winCount > loseCount) {
         console.log(winCount + " out of 5! You won the game! Congratulation!")
-    } else {
+    } else if (winCount < loseCount) {
         console.log(winCount + " out of 5! You lose the game! Better luck next time.")
-    }
-
+    } else {console.log("It's a draw! Click the button to try again!")}
 }
+
 
 let startButton = document.getElementById("inputButton");
 startButton.addEventListener("click", game);
-
-//click button to initiate game
-
-//play game function
-//  roundCount is less than 5
-//      play a round of game
-//          display the result of that round 
-//  if 
